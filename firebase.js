699745f41a6fde2d9ca2c08ea540d282070b5940
Moumenuwu <72457967+Moumenuwu@users.noreply.github.com>
2025-11-15 11:10:58 +0100

    Initialize Firebase app and Firestore/Auth services

--- firebase.js
+++ firebase.js
@@ -1,3 +1,8 @@
+// firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
    apiKey: "AIzaSyDilRX3cC45QIQvIlC70mB2N_gd0Pc7oAo",
   authDomain: "the-mmnger.firebaseapp.com",
@@ -6,3 +11,8 @@ const firebaseConfig = {
   messagingSenderId: "525558933868",
   appId: "1:525558933868:web:a9ded787b6350b72916b49"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
