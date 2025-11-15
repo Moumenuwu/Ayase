4b7d79512a549103e815e81600e1f7de5043424d
Moumenuwu <72457967+Moumenuwu@users.noreply.github.com>
2025-11-15 10:48:09 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyDilRX3cC45QIQvIlC70mB2N_gd0Pc7oAo",
+  authDomain: "the-mmnger.firebaseapp.com",
+  projectId: "the-mmnger",
+  storageBucket: "the-mmnger.firebasestorage.app",
+  messagingSenderId: "525558933868",
+  appId: "1:525558933868:web:a9ded787b6350b72916b49"
+};
